4dbba01cf2563373c11957a825fab5bc5ac40735
Di Wang <jiankong3@gmail.com>
2016-10-06 17:31:03 -0400

    Update Numpy.cmd

--- Numpy.cmd
+++ Numpy.cmd
@@ -3,3 +3,10 @@
 ###Convert Type
 1. Array.astype(np.uint8)
    np.asarray(a) #convert normal array to np array
+2. Construct Array
+   x = np.array([2,3,1,0])
+3. Reshape Array
+   x = np.rashape(x,(3,2))
+4.
+
+h. numpy.linalg.lstsq
